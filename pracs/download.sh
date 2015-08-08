0e08be75aae7494eab9adf8bc10879b581d1b3d8
Oliver Tan <sinovercosequalstan@gmail.com>
2015-08-08 22:57:17 +1000

    perms

--- pracs/download.sh
+++ pracs/download.sh
@@ -2,5 +2,5 @@
 
 FOLDER="prac2"
 
-cp -r ~otan/1927/$FOLDER .
-echo "Copied into $FOLDER. `cd $FOLDER` for the files, and use `ls` to see what is around."
+cp -r ~otan/1927/pracs/$FOLDER .
+echo "Copied into $FOLDER. cd $FOLDER for the files, and use ls to see what is around."
