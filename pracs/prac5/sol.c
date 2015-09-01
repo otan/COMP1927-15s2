95f636d66a12d83f2e5c14ee1e6c87d46d9ec391
Oliver Tan <sinovercosequalstan@gmail.com>
2015-09-01 13:45:18 +1000

    prac5 sol

---
+++ pracs/prac5/sol.c
@@ -0,0 +1,76 @@
+/* 
+ * Test files by Oliver Tan (otan@cse.unsw.edu.au)
+ *
+ * Question from 1927 13s2, Week 7 test (look where we are now)
+ * This IS a harder question. Probably the last linked list question!
+ *
+ * Complete partition(List l, int val), which takes in a list
+ * and returns a NEW list containing all items <= val, whilst
+ * the existing list only contains items > val
+ *
+ * For example:
+ * originalList: [0]->[4]->[3]->[6]->[5]->[2]->[X]
+ * partition(originalList, 3);
+ * newList (returned): [0]->[3]->[2]->[X]
+ * originalList (retained): [4]->[6]->[5]->[X]
+ *
+ * NOTE: order relative to both lists must be maintained!
+ *
+ * Compile: gcc -Wall -Werror -O -o testList testList.c list.c
+ * Run: ./testList
+*     
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+/* do you want the program to work? if so, don't change this */
+Node createNode(int i) {
+    Node n = malloc(sizeof(struct _node));
+    n->next = NULL;
+    n->data = i;
+    return n;
+}
+
+/* ... or this */
+List createList(void) {
+    List l = malloc(sizeof(*l));
+    l->first = NULL;
+    return l;
+}
+
+/* but do change this! */
+List partition(List l, int val) {
+    List l2 = createList();
+    Node prev = NULL;
+    Node curr = l->first;
+    Node ncurr = NULL;
+
+    while (curr != NULL) {
+        Node next = curr->next;
+        if (curr->data <= val) {
+            // update next list
+            if (ncurr == NULL) {
+                l2->first = curr;
+            } else {
+                ncurr->next = curr;
+            }
+            ncurr = curr;
+            curr->next = NULL;
+
+            // update current list
+            if (prev == NULL) {
+                l->first = next;
+            } else {
+                prev->next = next;
+            }
+        } else {
+            // prev only changes sometimes :)
+            prev = curr;
+        }
+        curr = next;
+    }
+
+    return l2;
+}
